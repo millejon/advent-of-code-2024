0c43f772d0d8330fce3d5ca11d3f7ffee2fce606
Jonathan Miller <millejon327@gmail.com>
2024-12-09 23:13:36 -0800

    Complete Part 2

--- day-3/mull_it_over.py
+++ day-3/mull_it_over.py
@@ -5,14 +5,17 @@ import re
 def parse_corrupted_data(data: str) -> None:
     corrupted_data = ""
     results = 0
+    enabled = True
 
-    valid_instruction = re.compile(
+    valid_instructions = re.compile(
         r"""
-        mul\(                           # Start of valid instruction
+        do\(\) |
+        don't\(\) |
+        mul\(                           # Start of valid multiply instruction
         (?P<x>\d{1,3})                  # First number
         ,                               # Comma delimiter
         (?P<y>\d{1,3})                  # Second number
-        \)                              # End of valid instruction
+        \)                              # End of valid multiply instruction
         """,
         re.VERBOSE,
     )
@@ -20,9 +23,14 @@ def parse_corrupted_data(data: str) -> None:
     with open(data, encoding="utf-8") as input:
         corrupted_data = input.read()
 
-    for instruction in valid_instruction.finditer(corrupted_data):
-        numbers = instruction.groupdict()
-        results += int(numbers["x"]) * int(numbers["y"])
+    for instruction in valid_instructions.finditer(corrupted_data):
+        if instruction.group() == "do()":
+            enabled = True
+        elif instruction.group() == "don't()":
+            enabled = False
+        elif enabled:
+            numbers = instruction.groupdict()
+            results += int(numbers["x"]) * int(numbers["y"])
 
     print(results)
 
